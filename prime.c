1a2ecdc604b709b3a2e72ded192a7581c01f7753
MadhuMathiShivamallu <30828443+MadhuMathiShivamallu@users.noreply.github.com>
2017-09-10 15:49:36 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int number,f1=1;
+printf("enter the number");
+scanf("%d",&number);
+while(number!=0)
+{
+f1=f1*number;
+number--;
+}
+printf("the factorial is %d",f1);
+return 0;
+}
+
