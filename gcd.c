781c4bdd18c491447bd734db7bb01a71f4c21618
MadhuMathiShivamallu <30828443+MadhuMathiShivamallu@users.noreply.github.com>
2017-09-12 16:03:03 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int number1,number2,gcd,i;
+    printf("enter the two number");
+    scanf("%d%d",&number1,&number2);
+    for(i=1;i<=number1&&i<=number2;++i)
+    {
+        if((number1%i==0)&&(number2%i==0))
+        {
+            gcd=i;
+            
+        }
+    }
+      printf("the gcd of two number is %d",gcd);
+}
