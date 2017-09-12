ab5a9b34bbf6941de3fb0c322949ecd702fb944a
MadhuMathiShivamallu <30828443+MadhuMathiShivamallu@users.noreply.github.com>
2017-09-12 15:16:17 +0530

    Rename fibonacci.c to fibonacci1.c

---
+++ fibonacci1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int number,temp,f1=0,f2=1,i;
+    printf("enter the range ");
+    scanf("%d",&number);
+    printf("the fibonacci number are");
+    printf("%d\n%d\n",f1,f2);
+    for(i=2;i<=number;i++)
+    {
+        temp=f1+f2;
+        printf("%d\n",temp);
+        f1=f2;
+        f2=temp;
+        
+    }
+    return 0;
+}
+    
+
+
+
+
+
+    
+
+
+
+
