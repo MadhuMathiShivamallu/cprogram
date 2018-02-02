ce4bb52156ebb716c2f108e8b3de39a779e419d4
MadhuMathiShivamallu <30828443+MadhuMathiShivamallu@users.noreply.github.com>
2018-02-02 14:14:13 +0530

    Create sorting_numberstring.c

---
+++ sorting_numberstring.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char string[50];
+    int count,i,j,t;
+    printf("enter number");
+    scanf("%[^\n]s",string);
+    count=strlen(string);
+    for(i=0;string[i]!='\0';i++)
+    {
+    for(j=i+1;j<count;j++)
+    {
+        if(string[i]>string[j])
+        {
+            t=string[i];
+            string[i]=string[j];
+            string[j]=t;
+        }
+    }   
+    }
+printf("%s",string);
+return 0;
+}
