bb83794b606320d67fd32bcffbb087b73fcce01c
MadhuMathiShivamallu <30828443+MadhuMathiShivamallu@users.noreply.github.com>
2018-02-02 13:56:35 +0530

    Create area_in_float.c

---
+++ area_in_float.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float length,breadth;
+    printf("enter length and breadth");
+    scanf("%f%f",&length,&breadth);
+    printf("area of land is %.5f",length*breadth);
+    return 0;
+}
