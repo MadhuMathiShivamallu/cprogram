0e05d09c8bd10837370edbd6f50b5e0c83864f55
MadhuMathiShivamallu <30828443+MadhuMathiShivamallu@users.noreply.github.com>
2018-02-06 10:23:19 +0000

    Create repeatednumber.c

---
+++ repeatednumber.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+  int array[100],number,i,t,j,count=0;
+  printf("enter total number");
+  scanf("%d",&number);
+  for(i=0;i<number;i++)
+  {
+    scanf("%d",&array[i]);
+    }
+    for(i=0;i<number-1;i++)
+    {
+      for(j=i+1;j<number;j++)
+      {
+        if(array[i]>array[j])
+        {
+          t=array[i];
+          array[i]=array[j];
+          array[j]=t;
+        } } }
+    for(i=0;i<number;i++)
+    {
+      if(array[i]==array[i+1])
+      {
+        printf(" %d ",array[i]);
+       count++; 
+      }
+    }
+    if(count==0)
+    {
+      printf("unique");
+    }
+    return 0;}
