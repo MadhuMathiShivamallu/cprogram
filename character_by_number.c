28ce977896fdc0f477c92f18dac624b747e04439
MadhuMathiShivamallu <30828443+MadhuMathiShivamallu@users.noreply.github.com>
2018-02-22 15:59:59 +0530

    Create character_by_number.c

---
+++ character_by_number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int number,i,num=65;
+    printf("enter number");
+    scanf("%d\t",&number);
+ for(i=1;i<number;i++)
+ {
+  num+=1;
+ }printf("%c",num);
+    return 0;
+}
