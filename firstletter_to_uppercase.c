53be32ed3eb511546357172fd2deb2b05c92907a
MadhuMathiShivamallu <30828443+MadhuMathiShivamallu@users.noreply.github.com>
2018-02-22 16:35:00 +0530

    Create firstletter_to_uppercase.c

---
+++ firstletter_to_uppercase.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char string[30];
+    int i;
+    printf("enter string");
+    scanf("%[^\n]s",string);
+    string[0]=string[0]-32;
+    for(i=0;string[i]!='\0';i++)
+    {
+        if(string[i]==' ')
+        {i++;
+            string[i]=string[i]-32;
+        }
+    }
+    printf("%s",string);
+    return 0;
+}
