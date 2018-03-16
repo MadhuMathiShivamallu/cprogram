a42c9494c88e75731a7b605e18d98e35be91bdbb
MadhuMathiShivamallu <30828443+MadhuMathiShivamallu@users.noreply.github.com>
2018-03-16 14:33:27 +0530

    Rename odd_factors.c to odd_factor.c

---
+++ odd_factor.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int number,i,count=0;
+  printf("enter the number");
+  scanf("%d",&number);
+  for(i=1;i<=number;i++)
+  {
+    if((number%i==0)&&(i%2!=0))
+    {
+       printf("%d ",i);
+    }
+      else
+      {
+    continue;
+      }
+   }return 0;
+  }
